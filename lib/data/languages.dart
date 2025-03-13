64d303376e5ec6bcc47a13157879feff08a291de
Raghav Shukla <raghav.shukla@neosoftmail.com>
2025-03-13 11:54:14 +0530

    rename variables, classes, files and directories.

---
+++ lib/data/languages.dart
@@ -0,0 +1,18 @@
+import '../model/language_model.dart';
+import 'c++/course.dart';
+import 'c/course.dart';
+import 'dart/dart_language.dart';
+import 'java/course.dart';
+import 'javascript/course.dart';
+import 'kotlin/course.dart';
+import 'swift/course.dart';
+
+const List<LanguageModel> languages = [
+  cCourse,
+  cppCourse,
+  dartLanguage,
+  javaCourse,
+  javascriptCourse,
+  kotlinCourse,
+  swiftCourse,
+];
--- lib/helper/language_helper.dart
+++ lib/helper/language_helper.dart
@@ -1,13 +1,6 @@
-import 'package:codekameleon/data/c++/course.dart';
-import 'package:codekameleon/data/c/course.dart';
-import 'package:codekameleon/data/java/course.dart';
-import 'package:codekameleon/data/javascript/course.dart';
-import 'package:codekameleon/data/kotlin/course.dart';
-import 'package:codekameleon/data/swift/course.dart';
-import 'package:codekameleon/preferences/preferences.dart';
-
-import '../data/dart/dart_language.dart';
+import '../data/languages.dart';
 import '../model/language_model.dart';
+import '../preferences/preferences.dart';
 
 class LanguageHelper {
   const LanguageHelper._();
@@ -15,23 +8,13 @@ class LanguageHelper {
   static LanguageModel? get recentLanguage {
     final String? recentLanguageName = Preferences.getRecentCourse();
     if (recentLanguageName == null) return null;
-    return _languages.firstWhere((e) => e.name == recentLanguageName);
+    return languages.firstWhere((e) => e.name == recentLanguageName);
   }
 
   static List<LanguageModel> get language {
     final String? recentLanguageName = Preferences.getRecentCourse();
 
-    if (recentLanguageName == null) return _languages;
-    return _languages.where((e) => e.name != recentLanguageName).toList();
+    if (recentLanguageName == null) return languages;
+    return languages.where((e) => e.name != recentLanguageName).toList();
   }
-
-  static const List<LanguageModel> _languages = [
-    cCourse,
-    cppCourse,
-    dartLanguage,
-    javaCourse,
-    javascriptCourse,
-    kotlinCourse,
-    swiftCourse,
-  ];
 }
