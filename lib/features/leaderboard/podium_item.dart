d5e0c48cc25689b910aaaf8090e9458b3803b0d6
Raghav Shukla <raghav.shukla@neosoftmail.com>
2025-03-17 16:37:30 +0530

    minor ui changes

--- lib/features/leaderboard/podium_item.dart
+++ lib/features/leaderboard/podium_item.dart
@@ -17,6 +17,7 @@ class PodiumItem extends StatelessWidget {
     final rankColor = _getRankColor(position, colorScheme);
 
     return Column(
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         CircleAvatar(
