dfb652f45d001be5cbcdb5976bd5ac9ef80dfe9e
raghav042 <raghvshukla042@gmail.com>
2025-03-18 06:51:41 +0530

    add user model class

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:codekameleon/extension/context_extension.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,36 +6,82 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final colorScheme = context.colorScheme;
     return Scaffold(
-      appBar: AppBar(title: const Text("My Profile")),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Hero(
+              tag: "my_profile_pic",
+              child: CircleAvatar(
+                radius: size.width / 4,
+              ),
+            ),
+            SizedBox(height: 10, width: double.maxFinite),
+            Text(
+              "Victor Timely",
+              style: TextStyle(fontSize: 35),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.maxFinite,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: colorScheme.primaryContainer,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Text(
+                "something about user useful information or anything else",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 40),
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Hero(
-                  tag: "my_profile_pic",
-                  child: CircleAvatar(
-                    radius: 60,
+                Container(
+                  width: size.width / 2 - 30,
+                  height: size.width / 2 - 30,
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceContainer,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Points",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: colorScheme.primary,
+                        ),
+                      ),
+                      Text(
+                        "225",
+                        style: TextStyle(
+                          fontSize: 50,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 10),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    Text(
-                      "Username surname",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                )
+                Container(
+                  width: size.width / 2 - 30,
+                  height: size.width / 2 - 30,
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceContainer,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
