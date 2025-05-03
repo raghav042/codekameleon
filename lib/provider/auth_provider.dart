d270ffaf573cd58298ec39c31d060b750c1870fd
Dhirenvinay <mailtoodhiren@gmail.com>
2025-05-03 10:28:49 +0530

    signup and auth provider functions

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,5 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
-class AuthenticationProvider extends ChangeNotifier{
+class AuthenticationProvider extends ChangeNotifier {
+  Future<void> createAccount(
+      {required String email, required String password}) async {
+    final credential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    // credential.user?
+  }
 
+  Future<void> loginAccount(
+      {required String email, required String password}) async {
+    final credential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    // credential.user?
+  }
+
+  Future<void> resetPassword(
+      {required String email, required String password}) async {
+    final credential =
+        await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+  }
 }
