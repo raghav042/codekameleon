3c4a1cc42d2aaca9b91965c36334b2b67e80989c
manu <heythisismansi@gmail.com>
2025-05-08 15:04:53 +0530

    completed the cpp folder

--- lib/data/c++/cpp_tutorials.dart
+++ lib/data/c++/cpp_tutorials.dart
@@ -1,6 +1,11 @@
+import 'package:codekameleon/data/c++/tutorials/basic_input_and_output.dart';
 import 'package:codekameleon/data/c++/tutorials/basics.dart';
 import 'package:codekameleon/data/c++/tutorials/data_types.dart';
+import 'package:codekameleon/data/c++/tutorials/if_else.dart';
+import 'package:codekameleon/data/c++/tutorials/loops.dart';
+import 'package:codekameleon/data/c++/tutorials/object_oriented.dart';
 import 'package:codekameleon/data/c++/tutorials/tokens.dart';
+import 'package:codekameleon/data/c++/tutorials/variable_scope.dart';
 import 'package:codekameleon/model/tutorial_model.dart';
 
 import '../c++/tutorials/introduction.dart';
@@ -30,6 +35,41 @@ const List<TutorialModel> cppTutorials = [
       duration: "4 min",
       content: data_types_in_cpp
   ),
+  TutorialModel(
+      title: "Variable Scope in C++",
+      description: "Variable scope in  C++",
+      duration: "5 min",
+      content: variable_scope_in_cpp
+  ),
+  TutorialModel(
+      title: "Basic Input and Output in C++",
+      description: "Basic Input and Output in C++",
+      duration: "4 min",
+      content: cppInputAndOutput
+  ),
+  TutorialModel(
+      title: "If else statement in C++",
+      description: "If else statement in C++",
+      duration: "6 min",
+      content: cppIfElse
+  ),
+  TutorialModel(
+      title: "Loops in C++",
+      description: "Loops in C++",
+      duration: "5 min",
+      content: cppLoops
+  ),
+  TutorialModel(
+      title: "Object Orientation in C++",
+      description: "Object Orientation in C++",
+      duration: "3 min",
+      content: cppObjectOrientation
+  ),
+
+
+
+
+
 
 
 
